e037079b3019a4f642e31b1c636a1c168cafa80d
TGLogic <20503664@student.uwa.edu.au>
2014-10-07 22:43:07 +0800

    Sorry Alex.  This didn't sync when I told it to.

--- image_processing/compile.sh
+++ image_processing/compile.sh
@@ -8,6 +8,8 @@
 
 #g++ bad_effects.cpp -o bad_effects -I/home/pi/raspicam_cv -L/home/pi/raspicam_cv -lraspicamcv -L/home/pi/userland/build/lib -lmmal_core -lmmal -lmmal_util -lvcos -lbcm_host `pkg-config --cflags --libs opencv`
 
-g++ quick_threshold.cpp -o quick_threshold -I/home/pi/raspicam_cv -L/home/pi/raspicam_cv -lraspicamcv -L/home/pi/userland/build/lib -lmmal_core -lmmal -lmmal_util -lvcos -lbcm_host `pkg-config --cflags --libs opencv`
+#g++ quick_threshold.cpp -o quick_threshold -I/home/pi/raspicam_cv -L/home/pi/raspicam_cv -lraspicamcv -L/home/pi/userland/build/lib -lmmal_core -lmmal -lmmal_util -lvcos -lbcm_host `pkg-config --cflags --libs opencv`
 
-g++ detect_red_object_cross.cpp -o detect_red_object_cross -I/home/pi/raspicam_cv -L/home/pi/raspicam_cv -lraspicamcv -L/home/pi/userland/build/lib -lmmal_core -lmmal -lmmal_util -lvcos -lbcm_host `pkg-config --cflags --libs opencv`
+#g++ detect_red_object_cross.cpp -o detect_red_object_cross -I/home/pi/raspicam_cv -L/home/pi/raspicam_cv -lraspicamcv -L/home/pi/userland/build/lib -lmmal_core -lmmal -lmmal_util -lvcos -lbcm_host `pkg-config --cflags --libs opencv`
+
+g++ RaspiCamTest2.cpp -o RaspiCamTest2 -I/home/pi/lib/robidouille/raspicam_cv -L/home/pi/lib/robidouille/raspicam_cv -lraspicamcv -L/home/pi/lib/userland/build/lib -lmmal_core -lmmal -lmmal_util -lvcos -lbcm_host `pkg-config --cflags --libs opencv`
