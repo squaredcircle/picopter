18b98f52066401eabbe4ecd3f5fcc0cdc409e5b4
TGLogic <20503664@student.uwa.edu.au>
2014-10-18 21:05:19 +0800

    Deleted old files
    I don't know why they didn't delete the last time I deleted them.

--- modules/include/define_globals.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DEFINE_GLOBALS_H_INCLUDED__
-#define __DEFINE_GLOBALS_H_INCLUDED__
-
-#include <string>
-
-int wp_it				= 0;
-bool repeatLoop			= true;
-
-#endif// __DEFINE_GLOBALS_H_INCLUDED__
